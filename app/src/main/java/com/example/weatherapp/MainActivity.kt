4061cc8d0721ce66565c87b7f1e09deb93cd7e6d
SemikAlexander <36263835+SemikAlexander@users.noreply.github.com>
2020-11-27 15:31:52 +0300

    toolbar

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,13 +1,8 @@
 package com.example.weatherapp
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity : AppCompatActivity(R.layout.activity_main)
 
-        //todo lol
-    }
-}
+//toast
+//snackbar
