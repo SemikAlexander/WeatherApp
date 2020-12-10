9f1fc4b61ef4b1bdc51028256c6df4aff07f916d
SemikAlexander <36263835+SemikAlexander@users.noreply.github.com>
2020-12-10 17:04:40 +0300

    Work with design. Add splash activity

--- app/src/main/java/com/example/weatherapp/dataClasses/Coord.kt
+++ app/src/main/java/com/example/weatherapp/dataClasses/Coord.kt
@@ -1,6 +1,6 @@
 package com.example.weatherapp.dataClasses
 
 data class Coord(
-    val lat: Int,
+    val lat: Double,
     val lon: Double
 )
--- app/src/main/java/com/example/weatherapp/dataClasses/Sys.kt
+++ app/src/main/java/com/example/weatherapp/dataClasses/Sys.kt
@@ -2,6 +2,6 @@ package com.example.weatherapp.dataClasses
 
 data class Sys(
     val country: String,
-    val sunrise: Int,
-    val sunset: Int
+    val sunrise: Long,
+    val sunset: Long
 )
